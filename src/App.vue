fd21d0d476f8a25570bc2e7392bd2250ad338d67
Andrei Balan <balanandreimgmail.com>
2020-10-10 21:49:25 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>Reddit</div>
+    <ol>
+      <li v-for="(post, i) in posts" :key="`${i}-${post.data.id}`">
+        <a :href="url + post.data.permalink">
+          <h3>{{ post.data.title }}</h3>
+          <img :src="post.data.thumbnail" />
+          <div>{{ post.data.subreddit_name_prefixed }}</div>
+        </a>
+      </li>
+    </ol>
+    <div ref="observeScroll"></div>
+    <button @click="loadMore">Load more</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      posts: [],
+      limitNumber: 25,
+      url: "https:///www.reddit.com",
+      subreddit: "aww",
+      afterId: null,
+    };
+  },
+  methods: {
+    getData() {
+      let apiUrl = `https://api.reddit.com/r/${this.subreddit}/top.json?limit=${this.limitNumber}&after=${this.afterId}`;
+
+      fetch(apiUrl)
+        .then((response) => response.json())
+        .then((response) => {
+          this.afterId = response.data.after;
+          this.posts = [...this.posts, ...response.data.children];
+        })
+        .then(() => this.observeScroll());
+    },
+
+    observeScroll() {
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.intersectionRatio === 1) {
+            observer.unobserve(entry.target);
+            console.log('intersectionRatio', entry.intersectionRatio);
+            this.getData();
+          }
+        });
+      });
+      observer.observe(this.$refs.observeScroll);
+    },
+
+    loadMore() {
+      this.getData();
+    },
+  },
+  created() {
+    this.getData();
+  },
+};
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
